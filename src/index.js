c10a2847285c1464a8fcc0a814426d2194591974
Mark Saunders <saunders99999@gmail.com>
2019-09-01 09:13:26 -0400

    rerender issue

--- src/index.js
+++ src/index.js
@@ -43,16 +43,43 @@ class QuizType extends React.Component {
   }
 }
 
+class Candy1 extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>You have selected: Snickers</p>
+        <div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Kickoff1 extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>You have selected: Snickers</p>
+        <div>
+        </div>
+      </div>
+    );
+  }
+}
+
 class Wizard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       quiztype: '',
-      value: ''
+      page: ''
     };
 
+    this.getNextPage = this.getNextPage.bind(this);
     this.handleQuizType = this.handleQuizType.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleHome = this.handleHome.bind(this);
+    this.handlePrevious = this.handlePrevious.bind(this);
+    this.handleNext = this.handleNext.bind(this);
   }
 
   handleQuizType(event) {
@@ -60,23 +87,66 @@ class Wizard extends React.Component {
     this.setState({quiztype: event.target.value});
   }
 
-  handleSubmit(event) {
-    console.log('submit');
-    event.preventDefault();
-    // this.setState({value: event.target.value});
+  handleHome() {
+    console.log('home');
+    this.setState({page: 'home'});
   }
 
+  handlePrevious() {
+    console.log('previous');
+    this.setState({page: 'candy1'});
+  }
+
+  handleNext() {
+    console.log('next');
+    this.setState({page: 'kickoff1'});
+  }
+
+  getNextPage() {
+    console.log(this.state.page);
+    if (this.state.page === 'candy1') {
+      return (
+        <Candy1 />
+      );
+    }
+
+    if (this.state.page === 'kickoff1') {
+      return (
+        <Kickoff1 />
+      );
+    }
+
+    // default
+    // return (
+    //   <QuizType quiztype={this.state.quiztype} 
+    //     onQuizChange={this.handleQuizType} />
+    // );
+  }
+  
+  // if (this.state.page === 'candy1') {
+  //   <Candy1 />
+  // }
+  
+  // if (this.state.page === 'kickoff1') {
+  //   <Kickoff1 />
+  // }
+
+
   render() {
+
+
     return (
-      <div>
+      <div className='wizard'>
         <h1>Wizard Demo</h1>
         <h2>The Wizard is a tool to help teams facilitate the right conversations</h2>
 
-        <form onSubmit={this.handleSubmit}>
-          <QuizType quiztype={this.state.quiztype}  
-                    onQuizChange={this.handleQuizType} />
-          <br />
-          <input type="submit" value="Submit" />
+        <form>
+          {this.getNextPage()}
+
+          <br />          
+          <button onClick={this.handleHome}>Home</button>
+          <button onClick={this.handlePrevious}>Previous</button>
+          <button onClick={this.handleNext}>Next</button>
         </form>
       </div>
     );
