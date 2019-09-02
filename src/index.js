74b07d8ebb4bda452795be31bd4568b429a6c166
Mark Saunders <saunders99999@gmail.com>
2019-09-01 23:11:19 -0400

    navigation working

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// for all pages
+const keyIndex = 'wizindex';
+const keyPage = 'wizpage';
+const keyQuiz = 'wizquiz';
+const pagesCandy = ['', 'Candy1', 'Candy2'];
+const pagesKickoff = ['', 'Kickoff1', 'Kickoff2'];
+
 class QuizType extends React.Component {
   constructor(props) {
     super(props);
@@ -23,7 +30,7 @@ class QuizType extends React.Component {
               type="radio" 
               name="quiztype" 
               value="candy" 
-              checked={this.props.quiztype === 'candy'}
+              checked={this.props.quiz === 'candy'}
               onChange={this.handleChange} />
           </label>
         </div>
@@ -34,7 +41,7 @@ class QuizType extends React.Component {
               type="radio" 
               name="quiztype" 
               value="kickoff" 
-              checked={this.props.quiztype === 'kickoff'}
+              checked={this.props.quiz === 'kickoff'}
               onChange={this.handleChange} />
           </label>
         </div>
@@ -47,7 +54,19 @@ class Candy1 extends React.Component {
   render() {
     return (
       <div>
-        <p>You have selected: Snickers</p>
+        <p>You have selected: Candy1</p>
+        <div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Candy2 extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>You have selected: Candy2</p>
         <div>
         </div>
       </div>
@@ -59,7 +78,7 @@ class Kickoff1 extends React.Component {
   render() {
     return (
       <div>
-        <p>You have selected: Snickers</p>
+        <p>You have selected: Kickoff1</p>
         <div>
         </div>
       </div>
@@ -67,81 +86,100 @@ class Kickoff1 extends React.Component {
   }
 }
 
-class Wizard extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quiztype: '',
-      page: ''
+      index: localStorage.getItem( keyIndex ) || 0,
+      page: localStorage.getItem( keyPage ) || '',
+      quiz: localStorage.getItem( keyQuiz ) || 'candy'
     };
 
-    this.getNextPage = this.getNextPage.bind(this);
+    this.getContent = this.getContent.bind(this);
     this.handleQuizType = this.handleQuizType.bind(this);
     this.handleHome = this.handleHome.bind(this);
     this.handlePrevious = this.handlePrevious.bind(this);
     this.handleNext = this.handleNext.bind(this);
+    this.setPageIndex = this.setPageIndex.bind(this);
+    this.setQuiz = this.setQuiz.bind(this);
   }
 
   handleQuizType(event) {
     console.log('event value', event.target.value);
-    this.setState({quiztype: event.target.value});
+    this.setQuiz(event.target.value);
   }
 
-  handleHome() {
-    console.log('home');
-    this.setState({page: 'home'});
-  }
-
-  handlePrevious() {
-    console.log('previous');
-    this.setState({page: 'candy1'});
-  }
-
-  handleNext() {
-    console.log('next');
-    this.setState({page: 'kickoff1'});
-  }
-
-  getNextPage() {
-    console.log(this.state.page);
-    if (this.state.page === 'candy1') {
-      return (
-        <Candy1 />
-      );
-    }
-
-    if (this.state.page === 'kickoff1') {
-      return (
-        <Kickoff1 />
+  getContent() {
+    if (this.state.page !== '') {
+      return(
+        <div>{this.state.page}</div>
       );
     }
 
     // default
-    // return (
-    //   <QuizType quiztype={this.state.quiztype} 
-    //     onQuizChange={this.handleQuizType} />
-    // );
+    return (
+      <QuizType quiz={this.state.quiz} 
+        onQuizChange={this.handleQuizType} />
+    );
   }
-  
-  // if (this.state.page === 'candy1') {
-  //   <Candy1 />
-  // }
-  
-  // if (this.state.page === 'kickoff1') {
-  //   <Kickoff1 />
-  // }
 
+  // ---------------------- utility
+  // haven't found the best way for React reseting state on page re-loads
+  setQuiz(quiz) {
+    this.setState({quiz: quiz});
+    localStorage.setItem(keyQuiz, quiz);
+  }
+
+  setPageIndex(index, page) {
+    this.setState({index: index, page: page});
+    localStorage.setItem(keyIndex, index);
+    localStorage.setItem(keyPage, page);
+  }
+
+  // ---------------------- nav buttons 
+  handleHome() {
+    console.log('home');
+    this.setPageIndex(0, pagesCandy[0]);
+    this.setQuiz('');
+  }
+
+  handlePrevious() {
+    console.log('quiz', this.state.quiz, 'index:', this.state.index)
+
+    if (this.state.quiz === 'candy' && this.state.index > 0) {
+      const newIndex = Number(this.state.index) - 1;
+      this.setPageIndex( newIndex, pagesCandy[newIndex]);
+    }
+
+    if (this.state.quiz === 'kickoff' && this.state.index > 0) {
+      const newIndex = Number(this.state.index) - 1;
+      this.setPageIndex( newIndex, pagesKickoff[newIndex]);
+    }
+  }
+
+  handleNext() {
+    console.log('quiz', this.state.quiz, 'index:', this.state.index)
+
+    if (this.state.quiz === 'candy' && this.state.index < (pagesCandy.length - 1)) {
+      const newIndex = Number(this.state.index) + 1;
+      this.setPageIndex( newIndex, pagesCandy[newIndex]);
+    }
+
+    if (this.state.quiz === 'kickoff' && this.state.index < (pagesKickoff.length - 1)) {
+      const newIndex = Number(this.state.index) + 1;
+      this.setPageIndex( newIndex, pagesKickoff[newIndex]);
+    }
+  }
 
   render() {
-
-
+    // const content = <Content page={this.state.page} />;
     return (
-      <div className='wizard'>
+      <div className='navpage'>
         <h1>Wizard Demo</h1>
         <h2>The Wizard is a tool to help teams facilitate the right conversations</h2>
 
         <form>
-          {this.getNextPage()}
+          <div id="content">{this.getContent()}</div>
 
           <br />          
           <button onClick={this.handleHome}>Home</button>
@@ -154,6 +192,6 @@ class Wizard extends React.Component {
 }
 
 ReactDOM.render(
-  <Wizard />,
+  <App />,
   document.getElementById('root')
 );
