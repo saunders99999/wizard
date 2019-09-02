a3ea6f0232bf1bfe8f60cf4da9da0a0e0d896010
Mark Saunders <saunders99999@gmail.com>
2019-09-02 07:17:08 -0400

    added candy questions

--- src/index.js
+++ src/index.js
@@ -53,11 +53,84 @@ class QuizType extends React.Component {
 class Candy1 extends React.Component {
   render() {
     return (
-      <div>
-        <p>You have selected: Candy1</p>
-        <div>
-        </div>
-      </div>
+<div>
+    <p>
+        <label>What kind of candy do you like?</label>
+    </p>
+    <div>
+      <label>
+        Milk
+        <input type="radio" name="candyColor" value="milk"/>
+      </label>
+    </div>
+    <div>
+      <label>
+        Sugar
+        <input type="radio" name="candyColor" value="sugar"/>
+      </label>
+    </div>
+    <div>
+      <label>
+        Dark
+        <input type="radio" name="candyColor" value="dark"/>
+      </label>
+    </div>
+    <p></p>
+    <p>
+        <label>Do you prefer Candy bar or Nugget type??</label>
+    </p>
+    <div>
+      <label>
+        Bar
+        <input type="radio" name="candyShape" value="bar"/>
+      </label>
+    </div>
+    <div>
+      <label>
+        Nuggets
+        <input type="radio" name="candyShape" value="nuggets"/>
+      </label>
+    </div>
+    <p></p>
+    <p>
+      <label>Do you prefer a filling or unfilled?</label>
+    </p>
+    <div>
+      <label>
+        Filled
+        <input type="radio" name="candyFilling" value="filled"/>
+      </label>
+    </div>
+    <div>
+      <label>
+        Unfilled
+        <input type="radio" name="candyFilling" value="unfilled"/>
+      </label>
+    </div>
+    <p></p>
+    <p>
+      <label>When you purchase candy which of the following is most important to you?</label>
+    </p>
+    <div>
+        <label>
+          Shape
+          <input type="radio" name="candyPref" value="shape"/>
+        </label>
+    </div>
+    <div>
+      <label>
+        Color
+        <input type="radio" name="candyPref" value="color"/>
+      </label>
+    </div>
+    <div>
+      <label>
+        Filling
+        <input type="radio" name="candyPref" value="filling"/>
+      </label>
+    </div>
+    <p></p>
+</div>
     );
   }
 }
@@ -66,7 +139,7 @@ class Candy2 extends React.Component {
   render() {
     return (
       <div>
-        <p>You have selected: Candy2</p>
+        <p>You have selected: Candy Box #3</p>
         <div>
         </div>
       </div>
@@ -79,6 +152,18 @@ class Kickoff1 extends React.Component {
     return (
       <div>
         <p>You have selected: Kickoff1</p>
+        <div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Kickoff2 extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>You have selected: Kickoff2</p>
         <div>
         </div>
       </div>
@@ -110,9 +195,27 @@ class App extends React.Component {
   }
 
   getContent() {
-    if (this.state.page !== '') {
+    if (this.state.page === 'Candy1') {
       return(
-        <div>{this.state.page}</div>
+        <Candy1 />
+      );
+    }
+
+    if (this.state.page === 'Candy2') {
+      return(
+        <Candy2 />
+      );
+    }
+
+    if (this.state.page === 'Kickoff1') {
+      return(
+        <Kickoff1 />
+      );
+    }
+
+    if (this.state.page === 'Kickoff2') {
+      return(
+        <Kickoff2 />
       );
     }
 
@@ -126,11 +229,13 @@ class App extends React.Component {
   // ---------------------- utility
   // haven't found the best way for React reseting state on page re-loads
   setQuiz(quiz) {
+    console.log(`quiz: ${quiz}`);
     this.setState({quiz: quiz});
     localStorage.setItem(keyQuiz, quiz);
   }
 
   setPageIndex(index, page) {
+    console.log(`index: ${index}, page: ${page}`);
     this.setState({index: index, page: page});
     localStorage.setItem(keyIndex, index);
     localStorage.setItem(keyPage, page);
@@ -138,14 +243,11 @@ class App extends React.Component {
 
   // ---------------------- nav buttons 
   handleHome() {
-    console.log('home');
     this.setPageIndex(0, pagesCandy[0]);
     this.setQuiz('');
   }
 
   handlePrevious() {
-    console.log('quiz', this.state.quiz, 'index:', this.state.index)
-
     if (this.state.quiz === 'candy' && this.state.index > 0) {
       const newIndex = Number(this.state.index) - 1;
       this.setPageIndex( newIndex, pagesCandy[newIndex]);
@@ -158,8 +260,6 @@ class App extends React.Component {
   }
 
   handleNext() {
-    console.log('quiz', this.state.quiz, 'index:', this.state.index)
-
     if (this.state.quiz === 'candy' && this.state.index < (pagesCandy.length - 1)) {
       const newIndex = Number(this.state.index) + 1;
       this.setPageIndex( newIndex, pagesCandy[newIndex]);
@@ -182,7 +282,7 @@ class App extends React.Component {
           <div id="content">{this.getContent()}</div>
 
           <br />          
-          <button onClick={this.handleHome}>Home</button>
+          <button onClick={this.handleHome}>Start Over</button>
           <button onClick={this.handlePrevious}>Previous</button>
           <button onClick={this.handleNext}>Next</button>
         </form>
